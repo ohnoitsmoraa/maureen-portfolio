27774594692652fc6c69437457aa0155a3ddf5df
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-11-05 21:06:19 +0300

    added cards folder to contain data to be held in the components

---
+++ src/data/ProjsData.jsx
@@ -0,0 +1,51 @@
+export const projsData = [
+	{
+		id: "proj-1",
+		imgUrl:
+			"https://plus.unsplash.com/premium_photo-1664302495846-8be51057b602?q=80&w=1148&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		title: "Geoda Green Suppliers",
+		desc: "A responsive web application, for the Geoda Green Suppliers company, to enhance marketing strategy for the company, as it deals with different farming strategies that enhance farmers' experience.",
+		github: "#",
+		live: "",
+		tech: ["Next.js,", "TailwindCSS,"],
+	},
+	{
+		id: "proj-2",
+		imgUrl:
+			"https://github.com/gafow/XOXO-Dating-Site-/blob/main/public/page-7.jpg?raw=true",
+		title: "XOXO - Dating - Site",
+		desc: " A modern dating platform designed to create meaningful connections between couples hence strengthening their bonds.",
+		github: "#",
+		live: "https://xoxo-dating-site.vercel.app/",
+		tech: [
+			"Socket.io,",
+			"TailwindCSS,",
+			"Node.js,",
+			"JSON WEB Token,"
+		],
+	},
+	{
+		id: "proj-3",
+		imgUrl:
+			"https://plus.unsplash.com/premium_photo-1682023587521-19219d9f8741?q=80&w=968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		title: "Bot-Battlr",
+		desc: "A responsive web application for a Bot company that accepts purchase of different bots and disqualification of bots. This web application can also serve as a back-end for a gaming server.",
+		github: "#",
+		live: "https://bot-battlr-black.vercel.app/",
+		tech: ["React.js,", "TailwindCSS,"],
+	},
+	{
+		id: "proj-4",
+		imgUrl:
+			"https://plus.unsplash.com/premium_photo-1661301075857-63868ae88c00?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		title: "Flatiron Bank",
+		desc: "An AI-powered app that easens the usage of bank applications like applying loans, making transactions and orders",
+		github: "#",
+		live: "https://bank-of-flatiron-silk.vercel.app/",
+		tech: [
+			"Next.js,",
+			"TailwindCSS,",
+			"Node.js,",
+		],
+	},
+];
---
+++ src/data/WorkData.jsx
@@ -0,0 +1,15 @@
+export const workData = [
+	{
+		title: "Sales Administrator",
+		company_name: "Geoda Green Suppliers Linited",
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1725022450/msgarage/ms_rfzeov.webp",
+		iconBg: "#383E56",
+		date: "January 2024 - Present",
+		points: [
+			"● Provided clerical and organizational support to sales team.",
+			"● Helped in management and creation of the company website",
+			"● Tracked orders and processed paper work",
+		],
+		supported: "",
+	},
+];
